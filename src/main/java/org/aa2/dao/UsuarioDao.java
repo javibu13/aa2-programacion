b6e95692ab1cfa57c542490331a47bc6ff3249e5
Javibu13 <javiersanzmoline@gmail.com>
2024-06-02 23:49:06 +0200

    Develop user list and details views and logic

--- src/main/java/org/aa2/dao/UsuarioDao.java
+++ src/main/java/org/aa2/dao/UsuarioDao.java
@@ -15,6 +15,9 @@ public interface UsuarioDao {
     @SqlQuery("SELECT COUNT(*) FROM Usuario")
     int getUsuariosCount();
 
+    @SqlQuery("SELECT * FROM Usuario WHERE Id LIKE CONCAT('%', :filtro, '%') OR Nombre LIKE CONCAT('%', :filtro, '%') OR Correo LIKE CONCAT('%', :filtro, '%') OR Rol LIKE :filtro")
+    List<Usuario> getUsuariosByFilter(@Bind("filtro") String filtro);
+
     @SqlQuery("SELECT * FROM Usuario WHERE Id = :id")
     Usuario getUsuarioById(@Bind("id") int id);
 
@@ -24,12 +27,18 @@ public interface UsuarioDao {
     @SqlUpdate("INSERT INTO Usuario (Nombre, Correo, Password, AvatarPath, Rol) VALUES (:nombre, :correo, :password, :avatarPath, :rol)")
     void addUsuario(@Bind("nombre") String nombre, @Bind("correo") String correo, @Bind("password") String password, @Bind("avatarPath") String avatarPath, @Bind("rol") String rol);
 
-    @SqlUpdate("UPDATE Usuario SET Nombre = :nombre, Correo = :correo, Password = :password, AvatarPath = :avatarPath, Rol = :rol WHERE Id = :id")
-    void updateUsuario(@Bind("id") int id, @Bind("nombre") String nombre, @Bind("correo") String correo, @Bind("password") String password, @Bind("avatarPath") String avatarPath, @Bind("rol") String rol);
+    @SqlUpdate("UPDATE Usuario SET Nombre = :nombre, Correo = :correo, Password = :password, Rol = :rol WHERE Id = :id")
+    void updateUsuario(@Bind("id") int id, @Bind("nombre") String nombre, @Bind("correo") String correo, @Bind("password") String password, @Bind("rol") String rol);
+
+    @SqlUpdate("UPDATE Usuario SET Nombre = :nombre, Correo = :correo, Rol = :rol WHERE Id = :id")
+    void updateUsuarioNoPass(@Bind("id") int id, @Bind("nombre") String nombre, @Bind("correo") String correo, @Bind("rol") String rol);
 
     @SqlUpdate("DELETE FROM Usuario WHERE Id = :id")
     void removeUsuario(@Bind("id") int id);
 
     @SqlQuery("SELECT COUNT(*) FROM Usuario WHERE Correo = :correo")
     boolean correoExists(@Bind("correo") String correo);
+
+    @SqlQuery("SELECT COUNT(*) FROM Usuario WHERE Id = :id")
+    boolean idExists(@Bind("id") int id);
 }
---
+++ src/main/java/org/aa2/servlet/UsuarioServlet.java
@@ -0,0 +1,152 @@
+package org.aa2.servlet;
+
+import org.aa2.dao.Database;
+import org.aa2.dao.UsuarioDao;
+import org.aa2.domain.Usuario;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+@WebServlet("/usuario")
+public class UsuarioServlet  extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            Usuario usuario = Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                return usuarioDao.getUsuarioById(Integer.parseInt(request.getParameter("id")));
+            });
+            request.setAttribute("usuario", usuario);
+            request.setAttribute("rolUsuario", request.getSession(false).getAttribute("rolUsuario"));
+            String stringStaticPath = request.getContextPath() + "/static/";
+            request.setAttribute("staticPath", stringStaticPath);
+            request.getRequestDispatcher("/detalleUsuario.jsp").forward(request, response);
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new ServletException("Error obteniendo usuarios", e);
+        }
+        }
+        
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getParameter("action").equals("create")) {
+            // CREATE
+            response.setContentType("text/html; charset=UTF-8");
+            String nombre = request.getParameter("nombre");
+            String correo = request.getParameter("correo");
+            String contrasena = request.getParameter("contrasena");
+            String rol = request.getParameter("rol");
+            
+            try {
+                boolean correoExists = Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                    return usuarioDao.correoExists(correo);
+                });
+
+                MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+                byte[] hashedBytes = messageDigest.digest(nombre.getBytes());
+                // Convert the byte array into a hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (byte b : hashedBytes) {
+                    sb.append(String.format("%02x", b));
+                }
+                String hashedNombre = sb.toString();
+                
+                if (correoExists) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("Correo ya registrado");
+                } else if (hashedNombre.equals(contrasena)) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("La contraseña no puede ser igual al nombre");
+                } else if (request.getSession(false).getAttribute("rolUsuario").equals("Usuario") && rol.equals("Administrador")) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("No tienes permisos para crear un usuario administrador");
+                } else {
+                    final String avatarPath = (rol.equals("Administrador")) ? "static/img/admin.png" : "static/img/user.png";
+                    Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                        usuarioDao.addUsuario(nombre, correo, contrasena, avatarPath, rol);
+                        return null;
+                    });
+                    response.setStatus(HttpServletResponse.SC_OK);
+                    response.getWriter().write("success");
+                }
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().write("Error durante el registro");
+            }
+        } else if (request.getParameter("action").equals("update")) {
+            // UPDATE
+            int id = Integer.parseInt(request.getParameter("id"));
+            String nombre = request.getParameter("nombre");
+            String correo = request.getParameter("correo");
+            String rol = request.getParameter("rol");
+
+            try {
+                boolean idExists = Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                    return usuarioDao.idExists(id);
+                });
+
+                boolean correoExists = Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                    return usuarioDao.correoExists(correo);
+                });
+                if (!idExists) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("No existe el usuario indicado");
+                } else if (request.getSession(false).getAttribute("rolUsuario").equals("Usuario") && Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                    return usuarioDao.getUsuarioById(id).getRol();
+                }).equals("Administrador")) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("No tienes permisos para modificar un usuario administrador");
+                } else if (correoExists && !Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                    return usuarioDao.getUsuarioById(id).getCorreo().equals(correo);
+                })) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("Correo ya registrado");
+                } else if (request.getSession(false).getAttribute("rolUsuario").equals("Usuario") && !rol.equals(Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                    return usuarioDao.getUsuarioById(id).getRol();
+                }))) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("No tienes permisos para cambiar el rol del usuario");
+                } else {
+                    Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                        usuarioDao.updateUsuarioNoPass(id, nombre, correo, rol);
+                        return null;
+                    });
+                    response.setStatus(HttpServletResponse.SC_OK);
+                    response.getWriter().write("success");
+                }
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().write("Error durante la actualización del usuario");
+            }
+
+        } else if (request.getParameter("action").equals("delete")) {
+            // DELETE
+            response.setContentType("text/html; charset=UTF-8");
+            int id = Integer.parseInt(request.getParameter("id"));
+            try {
+                boolean idExists = Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                    return usuarioDao.idExists(id);
+                });
+                if (!idExists) {
+                    response.setStatus(HttpServletResponse.SC_CONFLICT);
+                    response.getWriter().write("No existe el usuario indicado");
+                } else {
+                    Database.getInstance().withExtension(UsuarioDao.class, usuarioDao -> {
+                        usuarioDao.removeUsuario(id);
+                        return null;
+                    });
+                    response.setStatus(HttpServletResponse.SC_OK);
+                    response.getWriter().write("success");
+                }
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().write("Error durante la eliminación del usuario");
+            }
+        }
+    }
+}
